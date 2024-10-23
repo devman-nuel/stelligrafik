f02af32ce27ae682cff65756d8b77ab6c1bf9861
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2024-10-23 05:22:48 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useEffect } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import ScrollTop from './Components/Scrolltop';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import Home from './Pages/Home/Home';
 import './App.css';
+import scroll from "./Animations/Scroll";
+import { split } from "./Animations/Text";
+
+
+scroll();
+
+const Layout = () => {
+  useEffect(() => {
+    split(); // Initialize split text animation
+  }, []);
+
+  return (
+    <div className="main">
+      <ScrollTop />
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      }, 
+    
+    ]
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        Home
+        Home
+        Home
+        Home
+        Home
+        Home
+        Home
+    </div>
+  )
+}
+
+export default Home
